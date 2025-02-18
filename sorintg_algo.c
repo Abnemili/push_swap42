ff9a44b9c1de180c5f1f06e70b71319de5c01e58
Abdelmoula Nemili <abnemili@c3r4p2.1337.ma>
2025-02-18 15:49:08 +0100

    in progresse

--- sorintg_algo.c
+++ sorintg_algo.c
@@ -6,7 +6,7 @@
 /*   By: abnemili <abnemili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 15:39:37 by abnemili          #+#    #+#             */
-/*   Updated: 2025/02/18 15:45:44 by abnemili         ###   ########.fr       */
+/*   Updated: 2025/02/18 15:48:13 by abnemili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ void	sorting(t_node **stack_a, t_node **stack_b, int len)
 	else if (len == 4)
 		sort_4(stack_a, stack_b); //  done 
 	else if (len == 5)
-		sort_5(stack_a, stack_b);
+		sort_5(stack_a, stack_b); // done 
 	else
 		sort_stack(stack_a, stack_b, len);
 }
---
+++ sorting_utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnemili <abnemili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 15:46:54 by abnemili          #+#    #+#             */
+/*   Updated: 2025/02/18 15:47:05 by abnemili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "push_swap.h"
+
+void	ra_pb(t_node **stack_a, t_node **stack_b)
+{
+	ra(stack_a);
+	ra(stack_a);
+	pb(stack_a, stack_b);
+}
+
+void	rra_pb(t_node **stack_a, t_node **stack_b)
+{
+	rra(stack_a);
+	rra(stack_a);
+	pb(stack_a, stack_b);
+}
